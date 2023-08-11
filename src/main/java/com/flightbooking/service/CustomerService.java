7cf7a6714e57f2946617118c5d22d932d311718c
ShlomiLavi <shlomilavi50@gmail.com>
2023-08-11 21:08:56 +0300

    First Commit

--- src/main/java/com/flightbooking/service/CustomerService.java
+++ src/main/java/com/flightbooking/service/CustomerService.java
@@ -1,8 +1,19 @@
 package com.flightbooking.service;
 
+import com.flightbooking.web.dto.CardDto;
+import com.flightbooking.web.dto.FlightDto;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
 public interface CustomerService {
+    Optional<CardDto> getCard(UUID uuid);
 
+    List<FlightDto> getAllByDate(Date date);
 
+    List<FlightDto> getAllByPrice(double price);
 
-
+    void updateCustomer(UUID uuid);
 }
